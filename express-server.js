2851d0bb2f12a675f3c6c9c3f59cb2ad0fc676f8
Sri Nath <sri.developer731@gmail.com>
2018-10-30 15:17:26 -0400

    Add second route to display full url from a short url

--- express-server.js
+++ express-server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = 3030;
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -8,12 +8,23 @@ const urlDatabase = {
 };
 
 app.set('view engine', 'ejs');
+
+app.get('/urls/:id', (req, res) => {
+  let templateVars = {
+    shortUrl: req.params.id,
+    data: urlDatabase
+  };
+  res.render('urls-show', templateVars);
+});
+
 app.get('/', (req, res) => {
   res.send("Hello!");
 });
 
 app.get('/urls', (req, res) => {
-  let templateVars = { urls : urlDatabase };
+  let templateVars = {
+    urls: urlDatabase
+  };
   res.render('urls-index', templateVars);
 });
 
@@ -23,7 +34,7 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
-})
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
